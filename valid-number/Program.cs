2c8a014f56d604e113f786527765f2bf91a73749
John Gellert <gellertjm@gmail.com>
2019-09-05 15:14:21 -0500

    * .gitignore: * valid-number.sln: * valid-number.csproj: Initial commit
    * Program.cs: initial commit

---
+++ valid-number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace valid_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keepPlaying = true;
+            while (keepPlaying)
+            {
+                
+                Console.WriteLine("Enter any number between 1 and 10:");
+
+                int number = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine((number > 0 && number <= 10) ? "Valid" : "Invalid");
+
+                Console.WriteLine("Enter x to quit palying");
+                keepPlaying = (Console.ReadLine()) != "x" ? true : false ;
+            }
+        }      
+    }
+}
+
